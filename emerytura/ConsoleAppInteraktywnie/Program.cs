774724ce4979abed018afee7fba47f900be1f7bc
JacobDeMolay <jakubmolek2000@gmail.com>
2021-03-15 10:10:00 +0100

    Spersonalizowane powitanie

---
+++ emerytura/ConsoleAppInteraktywnie/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppInteraktywnie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witay ! ");
+            Console.WriteLine("Podaj swoje imie: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko: ");
+            string nazwisko = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie} {nazwisko}");
+        }
+    }
+}
